2410266b47a90df69728dd0b97c944c6df5ff97f
okaymisba <msbahsarfaraz@gmail.com>
2025-01-18 15:02:30 +0500

    Updated the regex pattern for query parsing.
    Handled the letter casing, spaces, line breaks and lastly added the mechanism for the extracting the boolean values from the SQL query but it is yet to be tested.

--- src/main/kotlin/org/example/database/queryParser/ParseQuery.kt
+++ src/main/kotlin/org/example/database/queryParser/ParseQuery.kt
@@ -6,19 +6,22 @@ object ParseQuery {
     fun parse(query: String?) {
 
         if (query != null) {
-            val regex = Regex("""INSERT INTO (\w+)\s*\((.*?)\)\s*VALUES\s*\((.*?)\);""")
+            val regex = Regex("""INSERT\s*INTO\s*(\w+)\s*\((.*?)\)\s*VALUES\s*\((.*?)\);""", RegexOption.IGNORE_CASE)
             val matchResult = regex.find(query)
 
             if (matchResult != null) {
                 val tableName = matchResult.groupValues[1]
-                val columns = matchResult.groupValues[2].split(", ").map {
+                val columns = matchResult.groupValues[2].split(",").map {
                     it.trim()
                 }
                 val column = columns.map { it.toString() }
-                val values = matchResult.groupValues[3].split(", ").map {
+                val values = matchResult.groupValues[3].split(",").map {
                     it.trim().removeSurrounding("'").let { value ->
                         if (value.startsWith("\"") && value.endsWith("\"")) {
                             value.removeSurrounding("\"")
+
+                        } else if (value == "true" || value == "false") {
+                            value.toBoolean()
 
                         } else if (value.matches("-?\\d+".toRegex())) {
                             value.toInt()
