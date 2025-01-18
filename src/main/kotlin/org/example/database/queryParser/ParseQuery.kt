59b9189dd5a89f0b1b6c7163b17859a56f196806
okaymisba <msbahsarfaraz@gmail.com>
2025-01-18 15:46:48 +0500

    Updated ParseQuery.kt to detect more than one queries.
    Used a when statement to check for the match of the type of query and then will handle the addition into the database.

--- src/main/kotlin/org/example/database/queryParser/ParseQuery.kt
+++ src/main/kotlin/org/example/database/queryParser/ParseQuery.kt
@@ -6,41 +6,40 @@ object ParseQuery {
     fun parse(query: String?) {
 
         if (query != null) {
-            val regex = Regex("""INSERT\s*INTO\s*(\w+)\s*\((.*?)\)\s*VALUES\s*\((.*?)\);""", RegexOption.IGNORE_CASE)
-            val matchResult = regex.find(query)
+            val insertRegex =
+                Regex("""INSERT\s*INTO\s*(\w+)\s*\((.*?)\)\s*VALUES\s*\((.*?)\);""", RegexOption.IGNORE_CASE)
+            when {
+                insertRegex.matches(query) -> {
+                    val matchResult = insertRegex.matchEntire(query)!!
+                    val tableName = matchResult.groupValues[1]
+                    val columns = matchResult.groupValues[2].split(",").map {
+                        it.trim()
+                    }
+                    val column = columns.map { it.toString() }
+                    val values = matchResult.groupValues[3].split(",").map {
+                        it.trim().removeSurrounding("'").let { value ->
+                            if (value.startsWith("\"") && value.endsWith("\"")) {
+                                value.removeSurrounding("\"")
 
-            if (matchResult != null) {
-                val tableName = matchResult.groupValues[1]
-                val columns = matchResult.groupValues[2].split(",").map {
-                    it.trim()
-                }
-                val column = columns.map { it.toString() }
-                val values = matchResult.groupValues[3].split(",").map {
-                    it.trim().removeSurrounding("'").let { value ->
-                        if (value.startsWith("\"") && value.endsWith("\"")) {
-                            value.removeSurrounding("\"")
+                            } else if (value == "true" || value == "false") {
+                                value.toBoolean()
 
-                        } else if (value == "true" || value == "false") {
-                            value.toBoolean()
+                            } else if (value.matches("-?\\d+".toRegex())) {
+                                value.toInt()
 
-                        } else if (value.matches("-?\\d+".toRegex())) {
-                            value.toInt()
+                            } else if (value.matches("-?\\d*\\.\\d+".toRegex())) {
+                                value.toDouble()
 
-                        } else if (value.matches("-?\\d*\\.\\d+".toRegex())) {
-                            value.toDouble()
+                            } else {
+                                value
 
-                        } else {
-                            value
+                            }
 
                         }
-
                     }
+
+                    InsertIntoTable.insert("testDatabase29Dec", tableName, column, values)
                 }
-
-                InsertIntoTable.insert("testDatabase29Dec", tableName, column, values)
-
-            } else {
-                println("Invalid query format")
             }
         }
     }
